55b7e79570b59e5d6adcfaaad2ae5e2a179b6d4e
chenallee <lchenalloy@gmail.com>
2020-01-14 01:10:43 -0500

    weather API call working

---
+++ script.js
@@ -0,0 +1,67 @@
+// DOM variables
+var $searchForm = document.querySelector("#city-search");
+var $searchInput = document.querySelector("#search-input");
+
+//
+var APIkey = "6aaa464bb00fa4a19aa146dac6e6844d";
+var urlStart = "http://api.openweathermap.org/data/2.5/";
+var searchHistory = [];
+
+/* ----- FUNCTIONS ----- */
+function searchHandler(searchTerm) {
+  //call currentweather & UV AJAX request
+  var searchCoords = currentweatherSearch(searchTerm);
+  //call 5Day AJAX request
+  fivedaySearch(searchTerm);
+}
+
+//call current weather API
+function currentweatherSearch(searchTerm) {
+  //how should we handle returning false?
+  console.log("Current| " + searchTerm);
+  var cityCoords;
+  var weatherUrl = urlStart + "weather?q=" + searchTerm + "&units=imperial&APPID=" + APIkey;
+
+  $.ajax({
+    url: weatherUrl,
+    method: "GET"
+  }).then(function (weatherResponse) {
+    console.log(weatherResponse);
+    //call function to create html elements showing response:
+
+    cityCoords = weatherResponse.coord;
+    currentUVSearch(cityCoords);
+  });  
+}
+
+//call current UV API
+function currentUVSearch(searchCoords) {
+  console.log("UV| ");
+  console.log(searchCoords);
+}
+
+//call forecast API
+function fivedaySearch(searchTerm) {
+  console.log("5| " + searchTerm);
+
+}
+/* ----- EVENT LISTENERS ----- */
+//on form submit, get text typed and then pass it to other functions
+$searchForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var searchTerm = $searchInput.value.trim();
+  if (!searchTerm) {
+    return false;
+  }
+  console.log(searchTerm);
+  //first: send it to search weather API
+  searchHandler(searchTerm);
+
+  //next: clear input
+  $searchInput.value = "";
+
+  //then: add term to history []
+
+  //finally: update history display
+});
+
