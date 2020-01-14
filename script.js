946a64469761787a6ecd5405de0c8693e3cd8fb7
chenallee <lchenalloy@gmail.com>
2020-01-14 01:55:34 -0500

    forecast API call working

--- script.js
+++ script.js
@@ -2,23 +2,32 @@
 var $searchForm = document.querySelector("#city-search");
 var $searchInput = document.querySelector("#search-input");
 
-//
+// variables for APIs
 var APIkey = "6aaa464bb00fa4a19aa146dac6e6844d";
 var urlStart = "http://api.openweathermap.org/data/2.5/";
+
+//
 var searchHistory = [];
 
 /* ----- FUNCTIONS ----- */
+//add search term to history [] & local storage
+
+//update search history display from local storage
+
+//handle clearing response divs and calling functions to search APIs
 function searchHandler(searchTerm) {
-  //call currentweather & UV AJAX request
+  //clear results divs:
+
+  //call currentweather & UV AJAX request:
   currentweatherSearch(searchTerm);
-  //call 5Day AJAX request
+  //call 5Day AJAX request:
   fivedaySearch(searchTerm);
 }
 
 //call current weather API
 function currentweatherSearch(searchTerm) {
   //how should we handle returning false?
-  console.log("Current| " + searchTerm);
+  //console.log("Current| " + searchTerm);
   var cityCoords;
   var weatherUrl = urlStart + "weather?q=" + searchTerm + "&units=imperial&APPID=" + APIkey;
 
@@ -48,6 +57,7 @@ function currentUVSearch(cityCoords) {
     method: "GET"
   }).then(function(uvResponse){
     console.log(uvResponse);
+    //call function to create html element for uv response:
   });
 
 }
@@ -56,7 +66,24 @@ function currentUVSearch(cityCoords) {
 function fivedaySearch(searchTerm) {
   console.log("5| " + searchTerm);
 
+  var forecastUrl = urlStart + "forecast?q=" + searchTerm + "&units=imperial&APPID=" + APIkey;
+
+  //maybe use 16-day instead and limit to 5 bc it's averages instead of 3-hr
+  $.ajax({
+    url: forecastUrl,
+    method: "GET"
+  }).then(function(forecastResponse){
+    console.log(forecastResponse);
+  });
+
 }
+
+//add current weather response to page:
+
+//add UV index response to page:
+
+//add 5Day forecast response to page:
+
 /* ----- EVENT LISTENERS ----- */
 //on form submit, get text typed and then pass it to other functions
 $searchForm.addEventListener("submit", function (event) {
